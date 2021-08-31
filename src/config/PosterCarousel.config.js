38d9ed11c630fae4bc74ed8b904c7e977bd09a5e
Bharti-28 <patilbharti959@gmail.com>
2021-08-31 14:38:09 +0530

    add poster slider

---
+++ src/config/PosterCarousel.config.js
@@ -0,0 +1,34 @@
+const settings = {
+    infinity: false,
+    autoplay: false,
+    slidesToShow: 5,
+    sliderToScroll: 2,
+    InitalSlide: 0,
+    responsive: [
+      {
+        breakpoints: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2,
+            infinite: true,
+        },
+      },
+      {
+        breakpoints: 600,
+        settings: {
+           slidesToShow: 2,
+           slidesToScroll: 1,
+           InitialSide: 1,
+        },
+      },
+      {
+        breakpoints: 480,
+        settings: {
+           slidesToShow: 2,
+           slidesToScroll: 1,
+        },
+      },
+    ],
+};
+
+export default settings;
